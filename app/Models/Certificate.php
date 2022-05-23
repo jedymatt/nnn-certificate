8a9bb3a8953ac654534b31bb40383169d0b7a654
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-24 01:40:29 +0800

    Create Certificate.php

---
+++ app/Models/Certificate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+class Certificate extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'validation_key',
+        'issue_date',
+    ];
+
+    protected $casts = [
+        'issue_date' => 'date',
+    ];
+}
