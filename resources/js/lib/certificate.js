a5093fe5d4499256642905e100d0eaa80f68405e
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-25 22:35:17 +0800

    Add QRCode to certificate

--- resources/js/lib/certificate.js
+++ resources/js/lib/certificate.js
@@ -1,12 +1,10 @@
 import { jsPDF } from 'jspdf'
 
-export default function generateCertificate({ name, description }) {
+export default function generateCertificate({ name, description, qrCode, validationKey }) {
     const doc = new jsPDF({
         orientation: 'landscape',
         unit: 'cm',
     });
-
-    doc.html('<h1>Certificate of Completion</h1>')
 
     doc.addFont('/fonts/Vollkorn/Vollkorn-VariableFont_wght.ttf', 'Vollkorn', 'normal');
     doc.addFont('/fonts/Roboto/Roboto-Regular.ttf', 'Roboto', 'normal');
@@ -14,21 +12,24 @@ export default function generateCertificate({ name, description }) {
     doc.addImage('/images/certificate.png', 'PNG', 0, 0, 29.7, 21);
 
     doc.setFont('Vollkorn')
-    doc.setFontSize(52);
-    doc.text(name, 14.8, 10.22, {
+        .setFontSize(52)
+        .text(name, 14.8, 10.22, {
+            align: 'center',
+        });
+
+    doc.setFont('Roboto')
+        .setFontSize(12)
+        .text(description, 14.8, 12.0, {
+            align: 'center',
+            lineHeightFactor: 1.52,
+        });
+
+
+    doc.addImage(qrCode, 'PNG', 13.2, 15.0, 3.4, 3.4);
+
+    doc.text(validationKey, 14.8, 19.0, {
         align: 'center',
     });
 
-    const desc = 'Porta egestas nibh risus aliquet. Ultricies sed eget tortor sit mi aliquam facilisis justo.\n' +
-        'Sollicitudin convallis odio dui feugiat leo gravida vestibulum. Viverra mi placerat quis eget\n' +
-        'risus morbi non faucibus. Dignissim sed egestas adipiscing est montes, eget ac.';
-
-    doc.setFont('Roboto')
-    doc.setFontSize(12);
-    doc.text(desc, 14.8, 12.0, {
-        align: 'center',
-        lineHeightFactor: 1.52,
-    })
-
-    doc.save('certificate.pdf');
+    return doc;
 }
