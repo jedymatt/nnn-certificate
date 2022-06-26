ee960089f7f47f50963aec38829f253b1200469c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-26 14:11:47 +0800

    Update CertificateController.php

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -3,19 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Models\Certificate;
+use App\Services\CertificateService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Date;
+use Inertia\Inertia;
 
 class CertificateController extends Controller
 {
+    public function create()
+    {
+        return Inertia::render('GenerateCertificatePage');
+    }
+
+    public function store(Request $request, CertificateService $certificateService)
+    {
+        $validated = $request->validate([
+            'display_name' => 'required|string',
+        ]);
+
+        $certificate = Certificate::create([
+            'display_name' => $validated['display_name'],
+            'validation_key' => $certificateService->generateValidationKey(),
+            'issue_date' => today(),
+        ]);
+
+        return redirect()->route('certificate.view', $certificate);
+    }
+
     public function show(Certificate $certificate)
     {
-        $november = today()->month(11);
-
-        $startOfNovember = $november->copy()->startOfMonth()->format('jS \d\a\y \o\f F, Y');
-        $endOfNovember = $november->copy()->endOfMonth()->format('jS \d\a\y \o\f F, Y');
-
-        return view('certificates.show', compact('certificate', 'startOfNovember', 'endOfNovember'));
+        return Inertia::render('ViewCertificatePage', [
+            'certificate' => $certificate,
+        ]);
     }
 }
