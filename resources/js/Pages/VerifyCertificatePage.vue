af202d27b455fdd55d93e4fceeabccd201319219
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-26 14:11:15 +0800

    Update VerifyCertificatePage.vue

---
+++ resources/js/Pages/VerifyCertificatePage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from "vue";
+import {Inertia} from "@inertiajs/inertia";
+
+const validationKey = ref('');
+
+const verify = () => {
+    console.log(validationKey.value);
+    Inertia.get(route('certificate.verify.result', {
+        certificate: validationKey.value,
+    }))
+}
+</script>
+
+<template>
+        <input v-model="validationKey" type="search">
+        <div>
+            <button @click="verify">Verify</button>
+        </div>
+</template>
