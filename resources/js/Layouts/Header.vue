288d5112f93159771c5ad735344c71992d605c73
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-02 14:15:23 +0800

    Update Header.vue

--- resources/js/Layouts/Header.vue
+++ resources/js/Layouts/Header.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {Link, Head} from '@inertiajs/inertia-vue3';
+import { Link, Head } from '@inertiajs/inertia-vue3';
+import GithubIcon from '@/Components/Icons/GitHubIcon.vue';
 
 defineProps({
     title: String,
@@ -7,17 +8,20 @@ defineProps({
 </script>
 
 <template>
-    <Head><title>{{ title }}</title></Head>
+
+    <Head>
+        <title>{{ title }}</title>
+    </Head>
     <header class="absolute top-0 inset-x-0">
-        <div class="container flex p-4 mx-auto"
-             :class="route().current('welcome') ? 'justify-end' : 'justify-between'"
-        >
+        <div class="container flex p-4 mx-auto" :class="route().current('welcome') ? 'justify-end' : 'justify-between'">
             <Link v-if="!route().current('welcome')" :href="route('welcome')"
-                  class="font-semibold text-indigo-500 hover:text-indigo-600">
-                NNN Certificate
+                class="font-bold text-indigo-500 rounded-md hover:text-indigo-600">
+            NNN Certificate
             </Link>
-            <a href="http://www.github.com/jedymatt/nnn-certificate" class="hover:underline" target="_blank"
-               rel="noopener">Github</a>
+            <a href="http://www.github.com/jedymatt/nnn-certificate"
+                class="hover:opacity-50 rounded-md inline-flex items-center justify-center" target="_blank" rel="noopener">
+                <GithubIcon class="h-5 w-5" />
+            </a>
         </div>
     </header>
 </template>
