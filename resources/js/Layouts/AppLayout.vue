926f96349819760c855685ec25e5d294566ed3dc
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-13 16:25:50 +0000

    Create AppLayout.vue

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HeaderLayout from './HeaderLayout.vue';
+</script>
+<template>
+    <div class="dark:bg-neutral-900 dark:text-white">
+        <HeaderLayout />
+        <slot></slot>
+    </div>
+</template>
